d1d34ff37bdb7c5a378490925287eb960fa43555
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-02-27 15:42:27 +0100

    first commit and finish project

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Image, Text } from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [img, setImg] = useState('');
+
+    const getCat = () => {
+        fetch('https://api.pexels.com/v1/curated', {
+            headers: {
+                'Authorization': 'dJPiCXI3dFo6JOPUPL3hpjlAzw0AycGgd55Q3wiG9JM3rdTTSqFaCuRn'
+            },
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            const randomIndex = Math.floor(Math.random() * data.photos.length);
+            const randomPhoto = data.photos[randomIndex];
+            setImg(randomPhoto.src.large);
+        })
+        .catch((error) => {
+            console.error('Erreur lors de la récupération de l\'image :', error);
+        });
+    }
+
+    useEffect(() => {
+        getCat();
+    }, []);
+
+    const handleNewImage = () => {
+        getCat();
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Une image de chat aléatoire</Text>
+            <Image source={{uri: img}} style={styles.img} />
+            <TouchableOpacity
+                style={styles.button}
+                onPress={handleNewImage}
+            >
+                <Text style={styles.buttonText}>Nouvelle image !</Text>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 50,
+        color: 'white',
+    },
+    img: {
+        width: '80%',
+        height: '45%',
+        marginBottom: 90,
+        borderRadius: 10,
+    },
+    button: {
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: 'white',
+    },
+    buttonText: {
+        color: 'black',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
 });
